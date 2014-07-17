4bd5f53a3f4e369cf715788d6d991847f78fcf49
Oliver Henlich <oliver.henlich@unimarket.com>
2014-07-18 11:41:38 +1200

    add unimarket test

--- src/test/java/com/philvarner/clamavj/test/UnimarketTestCase.java
+++ src/test/java/com/philvarner/clamavj/test/UnimarketTestCase.java
@@ -19,7 +19,8 @@ import java.util.concurrent.Executors;
 public class UnimarketTestCase {
     private static ClamScan scanner = new ClamScan("localhost", 3310, 60000);
     private static ExecutorService executorService = Executors.newCachedThreadPool();
-    private File file = new File("src/test/resources/eicar.txt");
+//    private File file = new File("src/test/resources/eicar.txt");
+    private File file = new File("src/test/resources/4.1.0_14660.zip");
 
     @BeforeClass
     public static void beforeClass() {
